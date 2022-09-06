87ef7e5ab816a63abff0375ee479ea21336a47b1
Nick Booher <njbooher@iastate.edu>
2022-09-06 13:58:07 -0500

    Use templates based on twig templates

--- apps.py
+++ apps.py
@@ -1,12 +1,13 @@
 from django.apps import AppConfig
 
 class MenuItem(object):
-    def __init__(self, text, url, children=None):
-        self.text = text
+    def __init__(self, text, url, newtab=False, children=None):
+        self.title = text
         self.url = url
         if children is None:
             children = []
         self.children = children
+        self.newtab = newtab
 
 class SocialMenuItem(MenuItem):
     def __init__(self, text, url, icon=None):
@@ -19,47 +20,78 @@ class Iastate22ThemeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'iastate22theme'
     html_title_tag = 'IASTATE22 Site Page Title'
-    header_parent_unit_name = 'IASTATE22 Parent Unit Name'
-    header_parent_unit_url = '#'
-    header_site_title = 'IASTATE22 Site Title'
+    parent_unit = {
+        "name": "IASTATE22 Parent Unit Name",
+        "url": "#"
+    }
+    website_title = {
+        "text": "IASTATE22 Site Title",
+        "url": "/"
+    }
     footer_site_title = 'IASTATE22 Site Name/Org Name Lorem Ipsum'
-    menu = [
-        MenuItem("Dropdown Menu", "/features", [
+    full_nav = [
+        MenuItem("Dropdown Menu", "/features", False, [
             MenuItem("Level 2 Page", "#"),
-            MenuItem("Level 2 Submenu", "#", [
-                MenuItem("Level 3 Submenu", "#", [
-                    MenuItem("Level 4 Page", "#"),
-                    MenuItem("Level 4 Page", "#"),
-                ]),
-                MenuItem("Level 3 Page", "#"),
-            ]),
             MenuItem("Level 2 Page", "#"),
             MenuItem("Level 2 Page", "#")
         ]),
         MenuItem("Link", "#")
     ]
-    header_site_links_menu = [
-        MenuItem("Sign Ons and Tools", "https://login.iastate.edu/"),
+    search_visible = True
+    utility_button = None
+    #utility_button = MenuItem("Button text", "#")
+    utility_nav = [
+        MenuItem("Info For...", "#", False, [
+            MenuItem("Current Students", "https://students.info.iastate.edu/"),
+            MenuItem("Faculty and Staff", "https://facultystaff.info.iastate.edu/")
+        ]),
+        MenuItem("Sign Ons", "https://login.iastate.edu/"),
     ]
-    footer_primary_links_menu = [
-        MenuItem("Sign Ons and Tools", "https://login.iastate.edu/"),
-    ]
-    footer_secondary_links_menu = [
-        MenuItem("Sign Ons and Tools", "https://login.iastate.edu/"),
-    ]
-    footer_social_media_menu = [
+    social_links = [
         SocialMenuItem("Facebook", "http://facebook.com/IowaStateU/", "fa-brands fa-facebook"),
         SocialMenuItem("Twitter", "http://twitter.com/iowastateu?lang=en", "fa-brands fa-twitter"),
         SocialMenuItem("Instagram", "http://instagram.com/iowastateu/", "fak fa-iastate22-instagram"),
         SocialMenuItem("YouTube", "http://youtube.com/user/iowastateu", "fa-brands fa-youtube"),
         SocialMenuItem("LinkedIn", "http://linkedin/iowastateu?lang=en", "fa-brands fa-linkedin-in"),
     ]
+    footer_nav = [
+        MenuItem("Footer Nav Item 1", "#"),
+        MenuItem("Footer Nav Item 2", "#"),
+        MenuItem("Footer Nav Item 3", "#"),
+        MenuItem("Footer Nav Item 4", "#"),
+    ]
+    footer_secondary_nav = [
+        MenuItem("Footer Secondary Nav Item 1", "#"),
+        MenuItem("Footer Secondary Nav Item 2", "#"),
+        MenuItem("Footer Secondary Nav Item 3", "#"),
+        MenuItem("Footer Secondary Nav Item 4", "#"),
+    ]
+    footer_utility_nav = [
+        MenuItem("Privacy Policy", "https://www.iastate.edu/disclaimers-and-terms"),
+        MenuItem("Non-discrimination Policy", "https://www.policy.iastate.edu/policy/discrimination"),
+        MenuItem("Digital Access and Accessibility", "https://www.digitalaccess.iastate.edu/"),
+        MenuItem("Consumer Information", "https://www.iastate.edu/consumer-information"),
+    ]
 
-    footer_contact_address = """
-        Building<br>
-        12345 Street Address<br>
-        Ames, IA 50011
-    """
+    site_org_contact = {
+        "name": "Site Org Contact Name",
+        "address": {
+            "building": "Building",
+            "street": "Street",
+            "city_state_zip": "Ames, IA 50011"
+        },
+        "phone": [
+            {
+                "label": "Phone Label",
+                "phone_number": "555-555-5555"
+            },
+        ],
+        "email": [
+            {
+                "label": "Email Label",
+                "email_address": "example@example.com"
+            },
+        ]
+    }
 
-    footer_contact_email = "email@iastate.edu"
-    footer_contact_phone = "(515) 294-4347"
+    copyright = "Iowa State University of Science and Technology"
--- context_processors.py
+++ context_processors.py
@@ -1,4 +1,4 @@
 from django.apps import apps
 
 def theme_config(request):
-    return {'iastate22theme_config': apps.get_app_config('iastate22theme')}
+    return {'isu22': apps.get_app_config('iastate22theme')}
